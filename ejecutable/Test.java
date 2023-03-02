6bee4c8c220c3b87ad9259206084984efb25526a
Alvoid101 <10101010alvoid@gmail.com>
2023-03-02 06:30:03 -0500

    Arreglando string

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -15,7 +15,7 @@ public class Test{
     //Input
     for(int i = 0; i<calorias.length; i++){
       calorias[i] = Integer.parseInt(JOptionPane.showInputDialog("Calorias del dia " + dias[i] +": "));
-      caloriasTotales += calorias[i];
+      promedio += (calorias[i]);
     }
     
     //Process
@@ -23,7 +23,7 @@ public class Test{
     while(count <= calorias.length){
       count ++;
       for(int i = 0; i<calorias.length; i++){
-	if((i+1)<calorias.length && calorias[i]<calorias[i+1]){
+	if((i+1)<calorias.length && calorias[i]>calorias[i+1]){
 	    menorCalorias = calorias[i];
 	    mayorCalorias = calorias[i+1];
 	    calorias[i]= mayorCalorias;
@@ -37,14 +37,13 @@ public class Test{
       }
     }
     //Promedio
-    promedio = caloriasTotales/7;
+    promedio /= 7;
 
     //Output
-    String header = new String();
     String row = new String();
     for(int i = 0; i<dias.length; i++){
      row += "|" + dias[i] + ": " + calorias[i] + "|";
     }
-    JOptionPane.showMessageDialog(null, "Orden de dia de mas a menos calorias:\n" + row);
+    JOptionPane.showMessageDialog(null, "Orden de dia de menos a mas calorias:\n" + row + "\nPromedio: " + promedio);
   }
 }
